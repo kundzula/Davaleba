3b38ee60578ce460874960ea627824011b091eb9
kundzula <maiairemadze2010@gmail.com>
2023-05-17 21:48:02 +0400

    lll

--- src/test/java/Proeqti.java
+++ src/test/java/Proeqti.java
@@ -1,15 +1,28 @@
 import Utils.ChromeStarter;
+import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Proeqti extends ChromeStarter {
     @Test
     public void proeqti (){
-        $("#search_list").setValue("მობილური").pressEnter();
-        sleep(2000);
-        $("#sort").selectOption("ფასი ზრდადობით");
-
+        $(by("src","/images/cart.png")).click();
+        $(byText("კალათა ცარიელია")).shouldBe(Condition.visible);
+        $(byText("შენახული ნივთები")).click();
+        $(byText("ვერ მოიძებნა")).shouldBe(Condition.visible);
+        $(byId("search_list")).setValue("კომპიუტერი").pressEnter();
+        $(byText("დაამატე კალათში"),0).click();
+        $(by("src","/images/cart.png")).click();
+        $(byClassName("save-icon")).click();
+        $(byText("შენახული ნივთები")).click();
+        $(byText("ყიდვა")).shouldBe(Condition.visible);
+        $(by("src","/images/cart.png")).click();
+        $(byText("კალათა ცარიელია")).shouldBe(Condition.visible);
+        $(byText("შენახული ნივთები")).click();
+        $(byClassName("fa-times")).click();
+        $(byText("ვერ მოიძებნა")).shouldBe(Condition.visible);
 
         }
 
--- src/test/java/Proeqti2.java
+++ src/test/java/Proeqti2.java
@@ -1,9 +1,23 @@
 import Utils.ChromeStarter;
+import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 
 public class Proeqti2 extends ChromeStarter {
     @Test
     public void proeqti2(){
+        $(by("src","/images/cart.png")).click();
+        $(byText("კალათა ცარიელია")).shouldBe(Condition.visible);
+        $(byId("search_list")).setValue("კომპიუტერი").pressEnter();
+        $(byText("დაამატე კალათში"),0).click();
+        $(by("src","/images/cart.png")).click();
+        $(byText("სრული ღირებულება")).shouldBe(Condition.visible);
+        $(byText("წაშლა")).click();
+        $(by("src","/images/cart.png")).click();
+        $(byText("კალათა ცარიელია")).shouldBe(Condition.visible);
 
     }
 }
